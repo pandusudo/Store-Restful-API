a961dbc4e0cc696ab192f0eed203825125b0a1a9
pandusudo <pandusudo@gmail.com>
2019-09-30 20:23:06 +0700

    fixing some bugs

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
+// import all required library
 const express = require('express')
 const app = express()
 const logger = require('morgan')
 const router = require('./src/index')
 const uuid = require('uuid/v1')
 const fileUpload = require('express-fileupload')
+const bodyParser = require('body-parser')
 require('dotenv/config')
 
-const bodyParser = require('body-parser')
-
+// use the library
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger('dev'))
 app.use(fileUpload())
 
+// port setting
 const port = process.env.SERVER_PORT || 5000
 
+// parent route
 app.use('/api', router)
 
+// listening to port
 app.listen(port, () => {
   console.log('listening to port ' + port)
 })
 
+// page not found handler
 app.get('*', (req, res) => {
   console.log('404 not found')
 })
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const Route = express.Router()
 
-// import product route
+// import all routes
 const products = require('./routes/products')
 const categories = require('./routes/categories')
 const users = require('./routes/users')
 
-// use product route
+// Routes
 Route.use('/products', products)
 Route.use('/categories', categories)
 Route.use('/users', users)
